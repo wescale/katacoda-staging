4d7805bea462ffe34b91da529ebb6f710a11b1e9
Pablo Lopez <plopez@xebia.fr>
2022-03-03 16:55:44 +0100

    minio in background

---
+++ argo-events-demo/background.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+
+docker pull alpine
+docker pull redis
+
+helm repo add minio https://charts.min.io/
+
+helm install  --set persistence.enabled=false \
+ --set buckets[0].name=input,buckets[0].policy=none,buckets[0].purge=false \
+ --set rootUser=rootuser,rootPassword=rootpass123 \
+--set replicas=4 \
+--set resources.requests.memory=500M \
+minio \
+minio/minio
+
+export POD_NAME=$(kubectl get pods --namespace default -l "release=minio" -o jsonpath="{.items[0].metadata.name}")
+
+kubectl port-forward $POD_NAME 9000 --namespace default &
+
+wget https://dl.min.io/client/mc/release/linux-amd64/mc
+chmod +x mc
+
+./mc config host add minio http://localhost:9000 $(kubectl get secret --namespace default minio -o jsonpath="{.data.rootUser}" | base64 --decode) $(kubectl get secret --namespace default minio -o jsonpath="{.data.rootPassword}" | base64 --decode)
+
+cat << EOF > secret-minio.yaml
+---
+apiVersion: v1
+data:
+  accesskey: cm9vdHVzZXI=
+  secretkey: cm9vdHBhc3MxMjM=
+kind: Secret
+metadata:
+  name: artifacts-minio
+  namespace: argo-events
+EOF
