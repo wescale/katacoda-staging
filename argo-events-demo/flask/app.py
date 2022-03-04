ec148634eea03a8c7ae872d728dc18a124878231
Pablo Lopez <plopez@xebia.fr>
2022-03-04 17:51:26 +0100

    Argo

--- argo-events-demo/flask/app.py
+++ argo-events-demo/flask/app.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template
+import flask
+
 import os
 
 INSIDE_OUT_FOLDER = os.path.join('static', 'inside_out')
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/flask/static", static_folder="static")
 app.config['INSIDE_OUT_FOLDER'] = INSIDE_OUT_FOLDER
+app_blueprint = flask.Blueprint("blueprint", __name__)
 
 image_name = 'sadness'
 
-@app.route('/')
+@app_blueprint.route('/')
 def show_index():
     global image_name
     full_filename = os.path.join(app.config['INSIDE_OUT_FOLDER'], image_name + '.jpg')
     return render_template("index.html", user_image = full_filename)
 
-@app.route('/admin/<emotion>')
+@app_blueprint.route('/admin/<emotion>')
 def change_image(emotion):
     global image_name
     image_name = emotion
@@ -23,4 +26,5 @@ def change_image(emotion):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.register_blueprint(app_blueprint, url_prefix="/flask")
+    app.run(host='0.0.0.0', ssl_context='adhoc')
