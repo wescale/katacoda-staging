00bc9ff4c9224a57b39833a3ba53f9bd2f76c90c
Pablo Lopez <plopez@xebia.fr>
2022-03-14 17:51:05 +0100

    Typo

--- argo-events-demo/assets/wait.sh
+++ argo-events-demo/assets/wait.sh
@@ -2,10 +2,10 @@
 
 show_progress()
 {
-  echo "Il reste 10 étapes à paramétrer avant de démarrer ce tuto\n\n"
+  echo "Il reste 9 étapes à paramétrer avant de démarrer ce tuto"
+  echo -n " "
 
-
-  echo -n "[Etape 1/10] K8S est en train de chauffer\n\n"
+  echo -n "[Etape 1/9] K8S est en train de chauffer"
   local -r pid="${1}"
   local -r delay='0.75'
   local spinstr='\|/-'
@@ -24,23 +24,22 @@ show_progress()
   done
   printf "    \b\b\b\b"
   echo ""
-  clear && echo -n "[Etape 2/10] Les noeuds rejoignent le cluster K8S\n\n"
+  clear && echo -n "[Etape 2/9] Les noeuds rejoignent le cluster K8S"
+  echo -n " "
 
   docker pull alpine
   docker pull redis
   docker pull quay.io/argoproj/argocli:latest
-  docker pull rg.fr-par.scw.cloud/katacoda/flask-argo:1.0.15
-  docker pull rg.fr-par.scw.cloud/katacoda/url-downloader:1.0.16
-  docker pull rg.fr-par.scw.cloud/katacoda/tesseract:1.0.5
 
   kubectl wait --for=condition=Ready nodes --all --timeout=120s
   ssh -q $(kubectl get node --selector='!node-role.kubernetes.io/master' -o jsonpath={.items[*].status.addresses[?\(@.type==\"InternalIP\"\)].address}) 'mkdir -p /root/.kube'
   scp -q /root/.kube/config $(kubectl get node --selector='!node-role.kubernetes.io/master' -o jsonpath={.items[*].status.addresses[?\(@.type==\"InternalIP\"\)].address}):/root/.kube/config
-  clear && echo -n "[Etape 3/10] Création du namespace dédié au tuto\n\n"
+  clear && echo -n "[Etape 3/9] Création du namespace dédié au tuto\n\n"
 
   kubectl create namespace argo-events
 
-  clear && echo -n "[Etape 4/10] Déploiement des CRDs de l'ingress Nginx\n\n"
+  clear && echo -n "[Etape 4/9] Déploiement des CRDs de l'ingress Nginx"
+  echo -n " "
 
   kubectl create namespace ingress-nginx
 
@@ -48,30 +47,34 @@ show_progress()
     --repo https://kubernetes.github.io/ingress-nginx \
     --namespace ingress-nginx --version='<4'
 
-  clear && echo -n "[Etape 5/10] Déploiement des compte de service Argo\n\n"
+  clear && echo -n "[Etape 5/9] Déploiement des compte de service Argo"
+  echo -n " "
   wget -q https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/argo-server/argo-server-sa.yaml
 
   kubectl apply -f argo-server-sa.yaml
 
-  clear && echo -n "[Etape 6/10] Déploiement de l'interface graphique ArgoServer\n\n"
+  clear && echo -n "[Etape 6/9] Déploiement de l'interface graphique ArgoServer"
+  echo -n " "
 
   kubectl apply -f deployment.yaml
 
-  clear && echo -n "[Etape 7/10] Déploiement des modules spécifiques au tutoriel\n\n"
-  kubectl apply -f deployment-flask.yaml
-
-  clear && echo -n "[Etape 8/10] Déploiement des services Argo\n\n"
+  clear && echo -n "[Etape 7/9] Déploiement des services Argo"
+  echo -n " "
 
   wget -q https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/argo-server/argo-server-service.yaml
   kubectl apply -f argo-server-service.yaml
 
-  clear && echo -n "[Etape 9/10] En attente de l'ingress Nginx\n\n"
+  clear && echo -n "[Etape 8/9] En attente de l'ingress Nginx"
+  echo -n " "
+
   kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=120s
 
-  clear && echo -n "[Etape 10/10] Creation d'un ingress pour ArgoServer\n\n"
+  clear && echo -n "[Etape 9/9] Creation d'un ingress pour ArgoServer"
+  echo -n " "
+
   kubectl apply -f ingress.yaml
 
   until kubectl get ingress --output=jsonpath='{.items[0].status.loadBalancer}' | grep "ingress"; do : sleep 1 ; done
