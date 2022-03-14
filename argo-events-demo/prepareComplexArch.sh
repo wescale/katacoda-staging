09c69e2c19cfd3908228aee9c209e837ce87c5ae
Pablo Lopez <plopez@xebia.fr>
2022-03-14 17:53:31 +0100

    Typo

---
+++ argo-events-demo/prepareComplexArch.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+docker pull rg.fr-par.scw.cloud/katacoda/flask-argo:1.0.15
+docker pull rg.fr-par.scw.cloud/katacoda/url-downloader:1.0.16
+docker pull rg.fr-par.scw.cloud/katacoda/tesseract:1.0.5
+
+kubectl apply -f deployment-flask.yaml
