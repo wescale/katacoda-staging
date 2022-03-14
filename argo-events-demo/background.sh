dfe9f7485cc08c6c6d2b7f8fdc7974a6c657317b
Pablo Lopez <plopez@xebia.fr>
2022-03-14 18:46:22 +0100

    Typo

--- argo-events-demo/background.sh
+++ argo-events-demo/background.sh
@@ -11,7 +11,7 @@ minio/minio >> /root/background.log
 wget https://dl.min.io/client/mc/release/linux-amd64/mc >> /root/background.log
 chmod +x mc >> /root/background.log
 
-./mc mb minio/input
+./mc mb minio/input >> /root/background.log
 
 sleep 5;
 
