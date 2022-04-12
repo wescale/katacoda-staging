21afa325355d678b5e41c4d224036d2576f2cfc1
Clément Loiselet <clement.loiselet@wescale.fr>
2022-04-12 23:31:38 +0200

    change init script

--- argo-rollouts/assets/00.init_script.sh
+++ argo-rollouts/assets/00.init_script.sh
@@ -3,8 +3,14 @@
 # Script played at the start of the katacoda course.
 # It should not be copied in the VM.
 
+echo "Waiting for files to be uploaded on the VM."
+
 # Wait for the Katacoda VM to start
-sleep 2;
+while [ ! -f ./00.init_script_runner.sh ]; do 
+    sleep 1; 
+done;
+
+echo "Files found. executing."
 
 # Execute the script on the runner
 ./00.init_script_runner.sh
